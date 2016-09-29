a5bf28fbc4c97c070272fd18c120eef703b60cc6
oklble <wangqb@szokl.com.cn>
2016-09-29 10:05:55 +0800

    enable watchdog for abnormal.
    enable IWDG in main, clear in ble sleep interrupt handling

--- BleDemoV1.0/Src/Main.c
+++ BleDemoV1.0/Src/Main.c
@@ -89,6 +89,19 @@ void main(void)
 			case Work_Mode_Eddystone_Uid:
 			case Work_Mode_Eddystone_Tlm:
 			case Work_Mode_Remote:
+				/* IWDG configuration: IWDG is clocked by LSI = 38KHz */
+                		/* IWDG timeout equal to 1.7 s (the timeout may varies due to LSI frequency dispersion) */
+                		/* IWDG timeout = (RELOAD_VALUE + 1) * Prescaler / LSI = (254 + 1) * 256 / 38 000 = 1717.6 ms */
+                		/* Enable the IWDG */
+                		IWDG_Enable();
+                		/* Enable write access to IWDG_PR and IWDG_RLR registers */
+                		IWDG_WriteAccessCmd(IWDG_WriteAccess_Enable);
+                		/* Set IWDG timeout */
+                		IWDG_SetPrescaler(IWDG_Prescaler_256);
+                		IWDG_SetReload(254); //RELOAD_VALUE
+                		/* Refresh IWDG */
+                		IWDG_ReloadCounter();
+  
 				//BLE initnal
 				BLE_Init();
 				//send 
